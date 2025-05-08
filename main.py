38d314db78741888057d5db8858c18fd2eebbe3b
ViscaB1899 <avichaid1996@gmail.com>
2025-05-08 15:04:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,5 @@ def webhook():
         return jsonify({"text": "אירעה שגיאה בעיבוד הבקשה."}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
